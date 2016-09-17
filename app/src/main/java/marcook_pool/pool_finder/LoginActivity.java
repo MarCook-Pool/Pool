340b148666e287045803fb3e93bde382b8126eb6
picklechips <martr4929@hotmail.com>
2016-09-17 12:36:46 -0400

    Added login activity and fragment

--- app/src/main/java/marcook_pool/pool_finder/LoginActivity.java
+++ app/src/main/java/marcook_pool/pool_finder/LoginActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
+    LoginFragment mLoginFragment = new LoginFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/marcook_pool/pool_finder/MainActivity.java
+++ app/src/main/java/marcook_pool/pool_finder/MainActivity.java
@@ -2,6 +2,7 @@ package marcook_pool.pool_finder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 

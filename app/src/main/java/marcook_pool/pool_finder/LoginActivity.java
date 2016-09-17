c7187acff2d3f011fa4408211ec44d00359a636c
Carson Cook <cook.carson38@gmail.com>
2016-09-17 12:58:24 -0400

    starts login activity from main activity

---
+++ app/src/main/java/marcook_pool/pool_finder/LoginActivity.java
@@ -0,0 +1,17 @@
+package marcook_pool.pool_finder;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Carson on 17/09/2016.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/marcook_pool/pool_finder/MainActivity.java
+++ app/src/main/java/marcook_pool/pool_finder/MainActivity.java
@@ -1,5 +1,6 @@
 package marcook_pool.pool_finder;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,25 +11,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-
-    FragmentManager mFragmentManager = getSupportFragmentManager();
-    TableLocationsFragment mTableLocationsFragment = new TableLocationsFragment();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button testButton = (Button) findViewById(R.id.test_button);
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView testText = (TextView) findViewById(R.id.textView);
-                testText.setText(getString(R.string.test_string));
-                mFragmentManager.beginTransaction().replace(R.id.activity_main, mTableLocationsFragment).commit();
-
-            }
-        });
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 

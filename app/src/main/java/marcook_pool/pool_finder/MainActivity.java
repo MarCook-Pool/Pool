ea66020a9c213f876687ac389be201f02956ef07
Carson Cook <cook.carson38@gmail.com>
2016-09-17 12:23:24 -0400

    examples

--- app/src/main/java/marcook_pool/pool_finder/MainActivity.java
+++ app/src/main/java/marcook_pool/pool_finder/MainActivity.java
@@ -1,13 +1,33 @@
 package marcook_pool.pool_finder;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button testButton = (Button) findViewById(R.id.test_button);
+    TextView testText = (TextView) findViewById(R.id.textView);
+
+    FragmentManager mFragmentManager = getSupportFragmentManager();
+    TableLocationsFragment mTableLocationsFragment = new TableLocationsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                testText.setText(getString(R.string.test_string));
+                mFragmentManager.beginTransaction().replace(R.id.activity_main, mTableLocationsFragment).commit();
+
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/marcook_pool/pool_finder/TableLocationsFragment.java
@@ -0,0 +1,20 @@
+package marcook_pool.pool_finder;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Carson on 17/09/2016.
+ */
+
+public class TableLocationsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}

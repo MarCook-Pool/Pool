bcf3eabc3f8589dfb464cbe7502d12f7f50f6184
Carson Cook <cook.carson38@gmail.com>
2016-09-17 13:25:08 -0400

    options menu created

--- app/src/main/java/marcook_pool/pool_finder/MainActivity.java
+++ app/src/main/java/marcook_pool/pool_finder/MainActivity.java
@@ -3,13 +3,14 @@ package marcook_pool.pool_finder;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     FragmentManager mFragmentManager = getSupportFragmentManager();
     TableLocationsFragment mTableLocationsFragment = new TableLocationsFragment();
@@ -18,18 +19,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button testButton = (Button) findViewById(R.id.test_button);
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView testText = (TextView) findViewById(R.id.textView);
-                testText.setText(getString(R.string.test_string));
-                mFragmentManager.beginTransaction().replace(R.id.activity_main, mTableLocationsFragment).commit();
-
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search:
+                //search for pool table locations
+                return true;
+            case R.id.settings:
+                //start settings activity
+                return true;
+            case R.id.review_app:
+                //send to play store or some review process
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }

3dbc2a54f10e57f7f7aa9af61b3f98bad78d9603
picklechips <martr4929@hotmail.com>
2016-09-18 05:54:22 -0400

    Added desperation output

--- app/src/main/java/marcook_pool/pool_finder/PoolLocationsFragment.java
+++ app/src/main/java/marcook_pool/pool_finder/PoolLocationsFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -29,14 +30,26 @@ public class PoolLocationsFragment extends Fragment {
     private DatabaseReference mDatabase;
     private static final String TAG = "PoolLocationsFragment";
 
+    TextView mOutput;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         //Query tableQuery = mDatabase.child("Tables").orderByChild("establishment");
+
+        return inflater.inflate(R.layout.fragment_pool_locations, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mOutput = (TextView) getView().findViewById(R.id.textView);
+
         mDatabase.child("Tables").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                String output = "";
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     PoolTable curTable = snapshot.getValue(PoolTable.class);
                     if (curTable == null) {
@@ -44,6 +57,8 @@ public class PoolLocationsFragment extends Fragment {
                         Log.e(TAG, "Pool table is unexpectedly null");
                     } else {
                         //TODO: Add card;
+                        output += curTable.establishment + ": " + curTable.description + "\n";
+                        mOutput.setText(output);
                         Log.d(TAG, "Pool Table: " + curTable.establishment);
                     }
                 }
@@ -54,6 +69,5 @@ public class PoolLocationsFragment extends Fragment {
                 Log.w(TAG, "PoolTableLocations: ", databaseError.toException());
             }
         });
-        return inflater.inflate(R.layout.fragment_pool_locations, container, false);
     }
 }

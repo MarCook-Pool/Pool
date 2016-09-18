467f3bf35349164960c0be4a4e37c6c8c433e78a
Carson Cook <cook.carson38@gmail.com>
2016-09-18 01:53:07 -0400

    views in submit location fragment

--- app/src/main/java/marcook_pool/pool_finder/SubmitLocationFragment.java
+++ app/src/main/java/marcook_pool/pool_finder/SubmitLocationFragment.java
@@ -1,20 +1,55 @@
 package marcook_pool.pool_finder;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 /**
  * Created by Carson on 17/09/2016.
  */
 
 public class SubmitLocationFragment extends Fragment {
+
+    Activity mActivity = getActivity();
+
+    Button mSubmitButton;
+    Button mLocationButton;
+    Button mPhotoButton;
+    EditText mEstablishment;
+    EditText mDescription;
+    RatingBar mRating;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_submit_locations, container, false);
+        View view = inflater.inflate(R.layout.fragment_submit_locations, container, false);
+        setViews();
+        setClickListeners();
+        return view;
+    }
+
+    private void setViews(){
+        mSubmitButton = (Button) getView().findViewById(R.id.submit);
+        mLocationButton = (Button) getView().findViewById(R.id.location);
+        mPhotoButton = (Button) getView().findViewById(R.id.add_photo_button);
+        mEstablishment = (EditText) getView().findViewById(R.id.establishment);
+        mDescription = (EditText) getView().findViewById(R.id.description);
+        mRating = (RatingBar) getView().findViewById(R.id.rating_bar);
+    }
+
+    private void setClickListeners(){
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
